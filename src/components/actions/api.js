d5cb737b40e2c42e61e6c09d33c8076d0a595c5b
KarthikrajS <karthikraj1995@gmail.com>
2020-06-03 09:04:38 +0530

    api

--- src/components/actions/api.js
+++ src/components/actions/api.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 export default{
     battleData:{
         battleDataList:()=>
-            axios.post("/battleData/list").then(res => res.data),
+            axios.post("/api/battleData/list").then(res => res.data),
         battleKingsList:()=>
-            axios.post("/battleData/kings").then(res => res.data),
+            axios.post("/api/battleData/kings").then(res => res.data),
         battleTypesList:()=>
-            axios.post("/battleData/types").then(res => res.data),
+            axios.post("/api/battleData/types").then(res => res.data),
         battleDataCount:data=>
-            axios.post("/battleData/count",{data}).then(res => res.data),
+            axios.post("/api/battleData/count",{data}).then(res => res.data),
         battleBasedOnLocation:location=>
-            axios.post("/battleData/battleBasedOnLocation",{location}).then(res => res.data),
+            axios.post("/api/battleData/battleBasedOnLocation",{location}).then(res => res.data),
         battleBasedOnTypes: type=>
-            axios.post("/battleData/battleBasedOnTypes",{type}).then(res => res.data),
+            axios.post("/api/battleData/battleBasedOnTypes",{type}).then(res => res.data),
         battleBasedOnKings: king=>
-            axios.post("/battleData/battleBasedOnKings",{king}).then(res => res.data),
+            axios.post("/api/battleData/battleBasedOnKings",{king}).then(res => res.data),
         battleDetails: battleName=>
-            axios.post("/battleData/battleDetails",{battleName}).then(res => res.data),
+            axios.post("/api/battleData/battleDetails",{battleName}).then(res => res.data),
         search:data=>
-            axios.post("/battleData/search",{data}).then(res=>res.data)
+            axios.post("/api/battleData/search",{data}).then(res=>res.data)
     }
 }
